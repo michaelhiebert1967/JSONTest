1bbaa9927a9db3b48dd7a9539a68cde2419b6708
Boba Fett <whiterabbit7844@aim.com>
2018-06-18 15:25:21 -0700

    Initial commit

---
+++ app/src/main/java/com/michaelhiebert/jsontest/JSONMainActivity.java
@@ -0,0 +1,13 @@
+package com.michaelhiebert.jsontest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class JSONMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jsonmain);
+    }
+}
